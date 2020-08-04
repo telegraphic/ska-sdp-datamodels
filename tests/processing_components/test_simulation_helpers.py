13dc200981d04ae08053dc84fd5c1549182a9a58
Tim Cornwell <realtimcornwell@gmail.com>
2020-08-04 10:34:42 +0100

    Correct handling of LOW  configurations

--- tests/processing_components/test_simulation_helpers.py
+++ tests/processing_components/test_simulation_helpers.py
@@ -12,23 +12,19 @@ from astropy.coordinates import SkyCoord
 from rascil.data_models.memory_data_models import Skycomponent
 from rascil.processing_components.simulation import create_named_configuration, plot_azel, plot_uvcoverage, \
     plot_uwcoverage, plot_vwcoverage
-from rascil.processing_components.visibility.base import create_blockvisibility
+from rascil.processing_components.visibility.base import create_blockvisibility, create_visibility
 
 
 class TestSimulationHelpers(unittest.TestCase):
     def setUp(self):
-        self.lowcore = create_named_configuration('MID')
+        self.lowcore = create_named_configuration('MID', rmax=10000.0)
         self.times = (numpy.pi / 12.0) * numpy.arange(-6.0, 6.0, 0.4)
-        self.frequency = numpy.linspace(1.0e9, 1.1e9, 3)
-        self.channel_bandwidth = numpy.array([1e8, 1e8, 1e8])
-        # Define the component and give it some spectral behaviour
-        f = numpy.array([100.0, 20.0, -10.0, 1.0])
-        self.flux = numpy.array([f, 0.8 * f, 0.6 * f])
+        self.frequency = numpy.array([2.998e8])
+        self.channel_bandwidth = numpy.array([1e8])
 
         # The phase centre is absolute and the component is specified relative (for now).
         # This means that the component should end up at the position phasecentre+compredirection
-        self.phasecentre = SkyCoord(ra=+0.0 * u.deg, dec=-90.0 * u.deg, frame='icrs', equinox='J2000')
-        self.comp = Skycomponent(direction=self.phasecentre, frequency=self.frequency, flux=self.flux)
+        self.phasecentre = SkyCoord(ra=+0.0 * u.deg, dec=-45.0 * u.deg, frame='icrs', equinox='J2000')
 
     def test_plotazel(self):
         self.vis = create_blockvisibility(self.lowcore, self.times, self.frequency,
@@ -39,8 +35,20 @@ class TestSimulationHelpers(unittest.TestCase):
         plot_azel([self.vis])
         plt.show(block=False)
 
-    def test_plot_uvcoverage(self):
+    def test_plot_uvcoverage_block(self):
         self.vis = create_blockvisibility(self.lowcore, self.times, self.frequency,
+                                          channel_bandwidth=self.channel_bandwidth,
+                                          phasecentre=self.phasecentre,
+                                          weight=1.0)
+        plt.clf()
+        plot_uvcoverage([self.vis])
+        plt.clf()
+        plot_uwcoverage([self.vis])
+        plt.clf()
+        plot_vwcoverage([self.vis])
+
+    def test_plot_uvcoverage(self):
+        self.vis = create_visibility(self.lowcore, self.times, self.frequency,
                                           channel_bandwidth=self.channel_bandwidth,
                                           phasecentre=self.phasecentre,
                                           weight=1.0)
