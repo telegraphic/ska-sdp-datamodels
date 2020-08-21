e1dcb17f1c7b4bc7a1b002322efe0f4958ba5310
Tim Cornwell <realtimcornwell@gmail.com>
2020-08-21 10:07:19 +0100

    Added notebook

--- tests/processing_components/test_visibility_xarray.py
+++ tests/processing_components/test_visibility_xarray.py
@@ -18,7 +18,7 @@ from rascil.processing_components.visibility.vis_xarray import convert_visibilit
 class TestXVisibility(unittest.TestCase):
     def setUp(self):
         self.lowcore = create_named_configuration('LOWBD2-CORE')
-        self.times = (numpy.pi / 43200.0) * numpy.arange(0.0, 300.0, 30.0)
+        self.times = (numpy.pi / 43200.0) * numpy.arange(-4*3600, +4*3600.0, 1800)
         self.frequency = numpy.linspace(1.0e8, 1.1e8, 3)
         self.channel_bandwidth = numpy.array([1e7, 1e7, 1e7])
         # Define the component and give it some spectral behaviour
@@ -43,9 +43,18 @@ class TestXVisibility(unittest.TestCase):
                                      weight=1.0)
         assert self.vis.nvis == len(self.vis.time)
         xvis = convert_visibility_to_xvisibility(self.vis)
+        print("\nInitial xvis")
         print(xvis)
-        assert xvis.sel({"polarisation":["XX", "YY"]}).data.shape[1] == 2
-        print(xvis.where(numpy.abs(xvis.uvw)< 10.0))
+        print("\nSlice")
+        print(xvis.data[100:110, 0:1])
+        print("\nBy polarisation")
+        assert xvis.sel({"polarisation":["XX", "YY"]})
+        print("\nBy antenna1")
+        print(xvis.where(xvis.antenna1<10).uvw)
+        print("\nBy uvwdist")
+        print(xvis.where(xvis.uvwdist<40.0).uvw)
+        print("\nBy time")
+        print(xvis.where(xvis.datetime>numpy.datetime64("2020-01-01T23:00:00")).datetime)
 
 if __name__ == '__main__':
     unittest.main()
