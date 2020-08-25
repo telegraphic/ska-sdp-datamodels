ed307f42a4fb6f9e6f79fd671704474889ec6c97
Tim Cornwell <realtimcornwell@gmail.com>
2020-08-25 11:37:32 +0100

    XVisibility class clearly defined

--- tests/processing_components/test_visibility_xarray.py
+++ tests/processing_components/test_visibility_xarray.py
@@ -8,17 +8,18 @@ import unittest
 import astropy.units as u
 import numpy
 from astropy.coordinates import SkyCoord
+from astropy.time import Time
 
 from rascil.data_models import Skycomponent, PolarisationFrame
 from rascil.processing_components.simulation import create_named_configuration
-from rascil.processing_components.visibility.base import create_visibility
-from rascil.processing_components.visibility.vis_xarray import convert_visibility_to_xvisibility
+from rascil.processing_components.visibility.base import create_blockvisibility
+from rascil.processing_components.visibility.vis_x import convert_blockvisibility_to_xvisibility
 
 
 class TestXVisibility(unittest.TestCase):
     def setUp(self):
-        self.lowcore = create_named_configuration('LOWBD2-CORE')
-        self.times = (numpy.pi / 43200.0) * numpy.arange(-4*3600, +4*3600.0, 1800)
+        self.lowcore = create_named_configuration('LOWBD2-CORE', rmax=1000)
+        self.times = (numpy.pi / 43200.0) * numpy.arange(-4 * 3600, +4 * 3600.0, 1800)
         self.frequency = numpy.linspace(1.0e8, 1.1e8, 3)
         self.channel_bandwidth = numpy.array([1e7, 1e7, 1e7])
         # Define the component and give it some spectral behaviour
@@ -33,28 +34,33 @@ class TestXVisibility(unittest.TestCase):
         self.compreldirection = self.compabsdirection.transform_to(pcof)
         self.comp = Skycomponent(direction=self.compreldirection, frequency=self.frequency, flux=self.flux)
     
-    
-    def test_convert_visibility_to_xarray(self):
-        self.vis = create_visibility(self.lowcore, self.times, self.frequency,
-                                     channel_bandwidth=self.channel_bandwidth,
-                                     phasecentre=self.phasecentre,
-                                     integration_time=30.0,
-                                     polarisation_frame=PolarisationFrame("linear"),
-                                     weight=1.0)
-        assert self.vis.nvis == len(self.vis.time)
-        xvis = convert_visibility_to_xvisibility(self.vis)
+    def test_convert_blockvisibility_to_xvisibility(self):
+        self.vis = create_blockvisibility(self.lowcore, self.times, self.frequency,
+                                          channel_bandwidth=self.channel_bandwidth,
+                                          phasecentre=self.phasecentre,
+                                          integration_time=30.0,
+                                          polarisation_frame=PolarisationFrame("linear"),
+                                          weight=1.0)
+
+        xvis = convert_blockvisibility_to_xvisibility(self.vis)
+        xvis = xvis.assign(uvdist=numpy.hypot(xvis.uvw[:, 0], xvis.uvw[:, 1]))
+
         print("\nInitial xvis")
         print(xvis)
-        print("\nSlice")
-        print(xvis.data[100:110, 0:1])
-        print("\nBy polarisation")
-        assert xvis.sel({"polarisation":["XX", "YY"]})
+        print("\nSlice of a DataArray")
+        print(xvis.vis[100:110, 0:1])
+        print("\nSelection of the Dataset by polarisation")
+        print(xvis.sel({"polarisation": ["XX", "YY"]}))
         print("\nBy antenna1")
-        print(xvis.where(xvis.antenna1<10).uvw)
-        print("\nBy uvwdist")
-        print(xvis.where(xvis.uvwdist<40.0).uvw)
+        print(xvis.where(xvis.antenna1 < 10).uvw)
+        print("\nBy uvdist")
+        print(xvis.where(xvis.uvdist < 40.0).uvw)
         print("\nBy time")
-        print(xvis.where(xvis.datetime>numpy.datetime64("2020-01-01T23:00:00")).datetime)
+        print(xvis.where(xvis.datetime > numpy.datetime64("2020-01-01T23:00:00")).datetime)
+        print("In bins of uvdist")
+        for result in xvis.groupby_bins("uvdist", bins=25):
+            print(result[0], result[1].sizes['stacked_time_spatial'])
+
 
 if __name__ == '__main__':
     unittest.main()
