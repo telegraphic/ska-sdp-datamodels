c8235e7b4297c5d357db8034f6d77bfb55474066
sddyates <sdaley@mrao.cam.ac.uk>
2021-02-22 17:28:24 +0000

    Added test and extended ci_checker_diagnostics to cover both residual and restored.

--- tests/apps/test_ci_imaging_checker.py
+++ tests/apps/test_ci_imaging_checker.py
@@ -3,6 +3,7 @@
 """
 import logging
 import sys
+import os
 
 import astropy.units as u
 import numpy
@@ -10,7 +11,7 @@ import pytest
 from astropy.coordinates import SkyCoord
 from numpy.random import default_rng
 
-from rascil.apps.ci_imaging_checker import cli_parser, analyze_image
+from rascil.apps.ci_imaging_checker import cli_parser, analyze_image, ci_checker_diagnostics, bdsf_qa_image
 from rascil.data_models.parameters import rascil_path
 from rascil.data_models.polarisation import PolarisationFrame
 from rascil.data_models.data_model_helpers import export_skycomponent_to_hdf5
@@ -24,18 +25,18 @@ log.addHandler(logging.StreamHandler(sys.stdout))
 
 
 @pytest.mark.parametrize("cellsize, npixel, flux_limit, insert_method, noise, tag", [
-#    (0.0001, 512, 0.001, "Nearest", 0.0, "nearest_512_nonoise"),
-    (0.0001, 512, 0.001, "Nearest", 0.00003, "nearest_512_noise_0.00003"),
-    (0.0001, 512, 0.0001, "Nearest", 0.00003, "nearest_512_noise_0.00003_flux0.0001"),
-    (0.0001, 512, 0.001, "Nearest", 0.0003, "nearest_512_noise_0.00003"),
-    (0.0001, 512, 0.001, "Lanczos", 0.0003, "nearest_512_noise_0.00003"),
-    (0.0001, 1024, 0.001, "Nearest", 0.00003, "nearest_1024_noise_0.00003")
+    # (0.0001, 512, 0.001, "Nearest", 0.0, "nearest_512_nonoise"),
+    (0.0001, 512, 0.001, "Nearest", 0.00003, "nearest_npixel512_noise0.00003_flux0.001"),
+    (0.0001, 512, 0.0001, "Nearest", 0.00003, "nearest_npixel512_noise0.00003_flux0.0001"),
+    (0.0001, 512, 0.001, "Nearest", 0.0003, "nearest_npixel512_noise0.00003_flux0.001"),
+    (0.0001, 512, 0.001, "Lanczos", 0.0003, "lanczos_npixel512_noise0.00003_flux0.001"),
+    (0.0001, 1024, 0.001, "Nearest", 0.00003, "nearest_npixel1024_noise0.00003_flux0.001")
 ])
 def test_continuum_imaging_checker(cellsize, npixel, flux_limit, insert_method, noise, tag):
     frequency = 1.e9
     phasecentre = SkyCoord(ra=+30.0 * u.deg, dec=-60.0 * u.deg, frame='icrs', equinox='J2000')
     hwhm_deg, null_az_deg, null_el_deg = find_pb_width_null(pbtype="MID", frequency=numpy.array([frequency]))
-    
+
     hwhm = hwhm_deg * numpy.pi / 180.0
     fov_deg = 8.0 * 1.36e9 / frequency
     pb_npixel = 256
@@ -43,25 +44,24 @@ def test_continuum_imaging_checker(cellsize, npixel, flux_limit, insert_method, 
     pb_cellsize = d2r * fov_deg / pb_npixel
     pbradius = 1.5
     pbradius = pbradius * hwhm
-    
+
     original_components = create_mid_simulation_components(phasecentre, numpy.array([frequency]), flux_limit,
                                                            pbradius, pb_npixel, pb_cellsize,
                                                            show=False, fov=10, apply_pb=True)
-    
-    
+
     components = original_components[0]
     components = sorted(components, key=lambda cmp: numpy.max(cmp.direction.ra))
-    
+
     comp_file = rascil_path(f"test_results/test_ci_checker_{tag}.hdf")
-    export_skycomponent_to_hdf5(components,comp_file)
-    
+    export_skycomponent_to_hdf5(components, comp_file)
+
     txtfile = rascil_path(f"test_results/test_ci_checker_{tag}.txt")
     f = open(txtfile, "w")
     f.write("# RA(deg), Dec(deg), Flux(Jy) \n")
     for cmp in components:
-          coord_ra = cmp.direction.ra.degree
-          coord_dec = cmp.direction.dec.degree
-          f.write("%.6f, %.6f, %10.6e \n"%(coord_ra, coord_dec, cmp.flux[0]))
+        coord_ra = cmp.direction.ra.degree
+        coord_dec = cmp.direction.dec.degree
+        f.write("%.6f, %.6f, %10.6e \n" % (coord_ra, coord_dec, cmp.flux[0]))
     f.close()
 
     model = create_image(npixel=npixel,
@@ -69,38 +69,40 @@ def test_continuum_imaging_checker(cellsize, npixel, flux_limit, insert_method, 
                          phasecentre=phasecentre,
                          frequency=numpy.array([frequency]),
                          polarisation_frame=PolarisationFrame("stokesI"))
-    
+
     model = insert_skycomponent(model, components, insert_method=insert_method)
-    
+
     if noise > 0.0:
         rng = default_rng()
         model["pixels"].data += rng.normal(0.0, noise, model["pixels"].data.shape)
-    
+
     model = smooth_image(model, width=1.0)
-    
+
     tagged_file = rascil_path(f"test_results/test_ci_checker_{tag}.fits")
     export_image_to_fits(model, tagged_file)
-    
+
     parser = cli_parser()
     args = parser.parse_args([
-        "--ingest_fitsname", tagged_file,
+        "--ingest_fitsname_restored", tagged_file,
+        "--ingest_fitsname_residual", tagged_file,
         "--finder_beam_maj", f"{numpy.rad2deg(cellsize)}",
         "--finder_beam_min", f"{numpy.rad2deg(cellsize)}",
-	"--check_source", f"True", 
-	"--input_source_format", f"external",
-	"--input_source_filename", comp_file, #txtfile
-	"--match_sep", f"1.e-3"])
-    
+        "--check_source", "True",
+        "--input_source_format", "external",
+        "--input_source_filename", comp_file,  # txtfile
+        "--match_sep", "1.0e-3"
+    ])
+
     out, matches_orig = analyze_image(args)
-    
+
     # check results directly
     sorted_comp = sorted(out, key=lambda cmp: numpy.max(cmp.direction.ra))
     log.info("Identified components:")
     for cmp in sorted_comp:
         coord_ra = cmp.direction.ra.degree
         coord_dec = cmp.direction.dec.degree
-        log.info("%.6f, %.6f, %10.6e \n"%(coord_ra, coord_dec, cmp.flux[0]))
-    
+        log.info("%.6f, %.6f, %10.6e \n" % (coord_ra, coord_dec, cmp.flux[0]))
+
     assert len(out) <= len(components)
     log.info("BDSF expected to find %d sources, but found %d sources" % (len(components), len(out)))
     matches_test = find_skycomponent_matches(out, components, tol=1e-3)
@@ -109,5 +111,7 @@ def test_continuum_imaging_checker(cellsize, npixel, flux_limit, insert_method, 
     for match in matches_test:
         log.info("%d %d %10.6e" % (match[0], match[1], match[2]))
 
-    numpy.testing.assert_array_almost_equal(matches_orig,matches_test)
+    numpy.testing.assert_array_almost_equal(matches_orig, matches_test)
 
+    assert os.path.exists(rascil_path(f"test_results/test_ci_checker_{tag}_restored_gaus_hist.png"))
+    assert os.path.exists(rascil_path(f"test_results/test_ci_checker_{tag}_residual_gaus_hist.png"))
