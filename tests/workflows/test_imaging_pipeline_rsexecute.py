cc582449a3794545d5eb3ca00fa734aa50d91b30
Tim Cornwell <realtimcornwell@gmail.com>
2021-02-26 14:53:25 +0000

    Test with/without Dask

--- tests/workflows/test_imaging_pipeline_rsexecute.py
+++ tests/workflows/test_imaging_pipeline_rsexecute.py
@@ -3,20 +3,19 @@
 
 import os
 import pprint
-import sys
-import unittest
 
 import numpy
+import pytest
 from astropy import units as u
 from astropy.coordinates import SkyCoord
 from matplotlib import pyplot as plt
 
-from rascil.data_models import SkyModel, export_skymodel_to_hdf5
+from rascil.data_models import export_skymodel_to_hdf5
 # These are the RASCIL functions we need
 from rascil.data_models.polarisation import PolarisationFrame
 from rascil.processing_components import show_image, export_image_to_fits, qa_image, \
-    create_low_test_image_from_gleam, create_image_from_visibility, advise_wide_field
-from rascil.workflows import invert_list_rsexecute_workflow, predict_list_rsexecute_workflow, \
+    create_low_test_image_from_gleam, create_image_from_visibility
+from rascil.workflows import predict_list_rsexecute_workflow, \
     simulate_list_rsexecute_workflow
 from rascil.workflows.rsexecute.execution_support.rsexecute import rsexecute
 from rascil.workflows.rsexecute.pipelines.pipeline_skymodel_rsexecute import \
@@ -29,144 +28,133 @@ import logging
 log = logging.getLogger("rascil-logger")
 
 logging.info("Starting imaging-pipeline")
-log.setLevel(logging.INFO)
+log.setLevel(logging.WARNING)
 
-
-class TestImagingPipeline(unittest.TestCase):
-    def setUp(self):
-        
-        rsexecute.set_client(use_dask=True)
-        
-        from rascil.data_models.parameters import rascil_path
-        self.dir = rascil_path('test_results')
-        
-        self.persist = os.getenv("RASCIL_PERSIST", False)
+@pytest.mark.parametrize("use_dask, optimise, component_threshold, test_max, test_min", [
+    (True,  True,  None, 4.093297359544571, -0.005846355119035153),
+    (True,  False, None, 4.093297359544571, -0.005846355119035153),
+    (False, False, None, 4.093297359544571, -0.005846355119035153),
+    (True,  True,  1.0,  4.093686139135822, -0.005874719205258523),
+    (True,  False, 1.0,  4.093686139135822, -0.005874719205258523),
+    (False, False, 1.0,  4.093686139135822, -0.005874719205258523),
+])
+def test_imaging_pipeline(use_dask, optimise, component_threshold, test_max, test_min):
+    rsexecute.set_client(use_dask=use_dask, optim=optimise)
     
-    def tearDown(self):
-        rsexecute.close()
+    from rascil.data_models.parameters import rascil_path
+    dir = rascil_path('test_results')
     
-    @unittest.skip("Non-deterministic")
-    def test_pipeline(self):
-        
-        nfreqwin = 5
-        ntimes = 5
-        rmax = 300.0
-        frequency = numpy.linspace(1e8, 1.2e8, nfreqwin)
-        if nfreqwin > 1:
-            channel_bandwidth = numpy.array(nfreqwin * [frequency[1] - frequency[0]])
-        else:
-            channel_bandwidth = numpy.array([2e7])
-        times = numpy.linspace(-numpy.pi / 3.0, numpy.pi / 3.0, ntimes)
-        phasecentre = SkyCoord(ra=+30.0 * u.deg, dec=-60.0 * u.deg, frame='icrs', equinox='J2000')
-        
-        bvis_list = simulate_list_rsexecute_workflow('LOWBD2',
-                                                     frequency=frequency,
-                                                     channel_bandwidth=channel_bandwidth,
-                                                     times=times,
-                                                     phasecentre=phasecentre,
-                                                     order='frequency',
-                                                     rmax=rmax, format='blockvis',
-                                                     zerow=False)
-        
-        log.info('%d elements in vis_list' % len(bvis_list))
-        log.info('About to make visibility')
-        bvis_list = rsexecute.compute(bvis_list, sync=True)
-        
-        advice_low = advise_wide_field(bvis_list[0], guard_band_image=8.0, delA=0.02)
-        advice_high = advise_wide_field(bvis_list[-1], guard_band_image=8.0, delA=0.02)
-        
-        npixel = advice_high['npixels2']
-        cellsize = min(advice_low['cellsize'], advice_high['cellsize'])
-        
-        gleam_model = [rsexecute.execute(create_low_test_image_from_gleam, nout=1)
-                       (npixel=npixel,
-                        frequency=[frequency[f]],
-                        channel_bandwidth=[channel_bandwidth[f]],
-                        cellsize=cellsize,
-                        phasecentre=phasecentre,
-                        polarisation_frame=PolarisationFrame("stokesI"),
-                        flux_limit=1.0,
-                        applybeam=True)
-                       for f, freq in enumerate(frequency)]
-        log.info('About to make GLEAM model')
-        gleam_model = rsexecute.persist(gleam_model)
-        
-        log.info('About to run predict to get predicted visibility')
-        future_vis_graph = rsexecute.scatter(bvis_list)
-        predicted_vislist = predict_list_rsexecute_workflow(future_vis_graph, gleam_model,
-                                                            context='ng')
-        predicted_vislist = rsexecute.persist(predicted_vislist)
-        
-        model_list = [rsexecute.execute(create_image_from_visibility, nout=1)(bvis_list[f],
-                                                                              npixel=npixel,
-                                                                              frequency=[frequency[f]],
-                                                                              channel_bandwidth=[channel_bandwidth[f]],
-                                                                              cellsize=cellsize,
-                                                                              phasecentre=phasecentre,
-                                                                              polarisation_frame=PolarisationFrame(
-                                                                                  "stokesI"),
-                                                                              chunksize=None)
-                      for f, freq in enumerate(frequency)]
-        
-        # Works ok if the model_list is precalculated
-        model_list = rsexecute.compute(model_list, sync=True)
-        
-        skymodel_list = [rsexecute.execute(SkyModel)(image=im) for im in model_list]
-        skymodel_list = rsexecute.persist(skymodel_list)
-        
-        continuum_imaging_list = \
-            continuum_imaging_skymodel_list_rsexecute_workflow(predicted_vislist,
-                                                               model_imagelist=model_list,
-                                                               skymodel_list=skymodel_list,
-                                                               context='ng',
-                                                               algorithm='mmclean',
-                                                               scales=[0],
-                                                               niter=100, fractional_threshold=0.1,
-                                                               threshold=0.01,
-                                                               nmoment=1,
-                                                               nmajor=5, gain=0.7,
-                                                               deconvolve_facets=4,
-                                                               deconvolve_overlap=32,
-                                                               deconvolve_taper='tukey',
-                                                               psf_support=64, do_wstacking=True,
-                                                               component_threshold=1.0,
-                                                               component_extraction="pixels")
-        
-        centre = nfreqwin // 2
-        continuum_imaging_list = rsexecute.compute(continuum_imaging_list, sync=True)
-        deconvolved = continuum_imaging_list[0][centre]
-        residual = continuum_imaging_list[1][centre]
-        restored = continuum_imaging_list[2][centre]
-        skymodel_list = continuum_imaging_list[3]
-        
-        export_skymodel_to_hdf5(skymodel_list, '%s/test-imaging-pipeline-dask_continuum_imaging_skymodel.hdf'
-                             % (self.dir))
-
-        
-        f = show_image(deconvolved, title='Clean image - no selfcal', cm='Greys',
-                       vmax=0.1, vmin=-0.01)
-        log.info(qa_image(deconvolved, context='Clean image - no selfcal'))
-        
-        plt.show()
-        
-        f = show_image(residual[0], title='Residual clean image - no selfcal', cm='Greys')
-        log.info(qa_image(residual[0], context='Residual clean image - no selfcal'))
-        plt.show()
-        export_image_to_fits(residual[0], '%s/test-imaging-pipeline-dask_continuum_imaging_residual.fits'
-                             % (self.dir))
-        
-        f = show_image(restored, title='Restored clean image - no selfcal',
-                       cm='Greys', vmax=1.0, vmin=-0.1)
-        log.info(qa_image(restored, context='Restored clean image - no selfcal'))
-        plt.show()
-        export_image_to_fits(restored, '%s/test-imaging-pipeline-dask_continuum_imaging_restored.fits'
-                             % (self.dir))
-        
-        qa = qa_image(restored, context='Restored clean image - no selfcal')
-        
-        assert abs(qa.data['max'] - 4.065534035591357) < 1e-7, str(qa)
-        assert abs(qa.data['min'] + 0.05201674672359368) < 1e-7, str(qa)
-
-
-if __name__ == '__main__':
-    unittest.main()
+    persist = os.getenv("RASCIL_PERSIST", False)
+    
+    nfreqwin = 5
+    ntimes = 5
+    rmax = 300.0
+    frequency = numpy.linspace(1e8, 1.2e8, nfreqwin)
+    if nfreqwin > 1:
+        channel_bandwidth = numpy.array(nfreqwin * [frequency[1] - frequency[0]])
+    else:
+        channel_bandwidth = numpy.array([2e7])
+    times = numpy.linspace(-numpy.pi / 3.0, numpy.pi / 3.0, ntimes)
+    phasecentre = SkyCoord(ra=+30.0 * u.deg, dec=-60.0 * u.deg, frame='icrs', equinox='J2000')
+    
+    bvis_list = simulate_list_rsexecute_workflow('LOWBD2',
+                                                 frequency=frequency,
+                                                 channel_bandwidth=channel_bandwidth,
+                                                 times=times,
+                                                 phasecentre=phasecentre,
+                                                 order='frequency',
+                                                 rmax=rmax, format='blockvis',
+                                                 zerow=False)
+    
+    bvis_list = rsexecute.persist(bvis_list)
+    
+    npixel = 512
+    cellsize = 1e-3
+    
+    gleam_model = [rsexecute.execute(create_low_test_image_from_gleam, nout=1)
+                   (npixel=npixel,
+                    frequency=[frequency[f]],
+                    channel_bandwidth=[channel_bandwidth[f]],
+                    cellsize=cellsize,
+                    phasecentre=phasecentre,
+                    polarisation_frame=PolarisationFrame("stokesI"),
+                    flux_limit=1.0,
+                    applybeam=True)
+                   for f, freq in enumerate(frequency)]
+    log.info('About to make GLEAM model')
+    gleam_model = rsexecute.persist(gleam_model)
+    
+    predicted_vislist = predict_list_rsexecute_workflow(bvis_list, gleam_model, context='ng')
+    predicted_vislist = rsexecute.persist(predicted_vislist)
+    
+    model_list = [rsexecute.execute(create_image_from_visibility, nout=1)
+                  (bvis_list[f],
+                   npixel=npixel,
+                   frequency=[frequency[f]],
+                   channel_bandwidth=[channel_bandwidth[f]],
+                   cellsize=cellsize,
+                   phasecentre=phasecentre,
+                   polarisation_frame=PolarisationFrame("stokesI"),
+                   chunksize=None)
+                  for f, freq in enumerate(frequency)]
+    
+    model_list = rsexecute.persist(model_list)
+    
+    continuum_imaging_list = \
+        continuum_imaging_skymodel_list_rsexecute_workflow(predicted_vislist,
+                                                           model_imagelist=model_list,
+                                                           skymodel_list=None,
+                                                           context='ng',
+                                                           algorithm='mmclean',
+                                                           scales=[0],
+                                                           niter=100, fractional_threshold=0.1,
+                                                           threshold=0.01,
+                                                           nmoment=1,
+                                                           nmajor=5, gain=0.7,
+                                                           deconvolve_facets=4,
+                                                           deconvolve_overlap=32,
+                                                           deconvolve_taper='tukey',
+                                                           psf_support=64, do_wstacking=True,
+                                                           component_threshold=None,
+                                                           component_extraction="pixels")
+    
+    centre = nfreqwin // 2
+    continuum_imaging_list = rsexecute.compute(continuum_imaging_list, sync=True)
+    deconvolved = continuum_imaging_list[0][centre]
+    residual = continuum_imaging_list[1][centre]
+    restored = continuum_imaging_list[2][centre]
+    skymodel_list = continuum_imaging_list[3]
+    
+    export_skymodel_to_hdf5(skymodel_list, '%s/test-imaging-pipeline-dask_continuum_imaging_skymodel.hdf'
+                            % (dir))
+    
+    f = show_image(deconvolved, title='Clean image - no selfcal', cm='Greys',
+                   vmax=0.1, vmin=-0.01)
+    log.info(qa_image(deconvolved, context='Clean image - no selfcal'))
+    export_image_to_fits(deconvolved, '%s/test-imaging-pipeline-dask_continuum_imaging_clean.fits'
+                         % (dir))
+    
+    plt.show()
+    
+    f = show_image(residual[0], title='Residual clean image - no selfcal', cm='Greys')
+    log.info(qa_image(residual[0], context='Residual clean image - no selfcal'))
+    plt.show()
+    export_image_to_fits(residual[0], '%s/test-imaging-pipeline-dask_continuum_imaging_residual.fits'
+                         % (dir))
+    
+    f = show_image(restored, title='Restored clean image - no selfcal',
+                   cm='Greys', vmax=1.0, vmin=-0.1)
+    log.info(qa_image(restored, context='Restored clean image - no selfcal'))
+    plt.show()
+    export_image_to_fits(restored, '%s/test-imaging-pipeline-dask_continuum_imaging_restored.fits'
+                         % (dir))
+    
+    qa = qa_image(restored, context='Restored clean image - no selfcal')
+    
+    # Correct serial values for skycomponent extraction
+    # assert abs(qa.data['max'] - 4.093686139135822) < 1e-7, str(qa)
+    # assert abs(qa.data['min'] + 0.005874719205258523) < 1e-7, str(qa)
+    
+    # Correct values for no skycomponent extraction
+    assert abs(qa.data['max'] - test_max) < 1e-7, str(qa)
+    assert abs(qa.data['min'] - test_min) < 1e-7, str(qa)
