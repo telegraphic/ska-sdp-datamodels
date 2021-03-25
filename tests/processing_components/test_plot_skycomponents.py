a4daee476b227102a5ead4d7e86dcee08a862fcb
Celeste Lu <yl790@cam.ac.uk>
2021-03-25 13:44:08 +0000

    Modified test_plot_skycomponents.py

--- tests/processing_components/test_plot_skycomponents.py
+++ tests/processing_components/test_plot_skycomponents.py
@@ -18,9 +18,7 @@ from numpy.testing import assert_array_almost_equal
 from rascil.data_models import PolarisationFrame, rascil_path, rascil_data_path
 from rascil.processing_components.simulation import (
     create_low_test_skycomponents_from_gleam,
-)
-from rascil.processing_components.skycomponent.operations import (
-    filter_skycomponents_by_flux,
+    addnoise_skycomponent,
 )
 from rascil.processing_components.skycomponent.plot_skycomponent import (
     plot_skycomponents_positions,
@@ -53,61 +51,76 @@ class TestPlotSkycomponent(unittest.TestCase):
             polarisation_frame=PolarisationFrame("stokesI"),
             radius=0.5,
         )
-        self.plot_file = self.dir+"/test_plot_skycomponents"
+        self.noise = 1.0e-6
+        self.plot_file = self.dir + "/test_plot_skycomponents"
 
     def test_plot_positions(self):
 
+        comp_test = addnoise_skycomponent(
+            self.components, noise=self.noise, mode="direction"
+        )
         ra_test, dec_test = plot_skycomponents_positions(
-            self.components[: len(self.components) // 2],
+            comp_test,
             self.components,
             plot_file=self.plot_file,
         )
 
         assert len(ra_test) == len(dec_test)
-        assert len(ra_test) == len(self.components) // 2
+        assert len(ra_test) == len(comp_test)
 
     def test_plot_position_distance(self):
 
+        comp_test = addnoise_skycomponent(
+            self.components, noise=self.noise, mode="direction"
+        )
         ra_error, dec_error = plot_skycomponents_position_distance(
-            self.components[: len(self.components) // 2],
+            comp_test,
             self.components,
             self.phasecentre,
             plot_file=self.plot_file,
         )
-       
-        assert_array_almost_equal(ra_error, 0.0)
-        assert_array_almost_equal(dec_error, 0.0)
-        assert len(ra_error) == len(self.components) // 2
+
+        assert_array_almost_equal(ra_error, 0.0, decimal=3)
+        assert_array_almost_equal(dec_error, 0.0, decimal=3)
+        assert len(ra_error) == len(comp_test)
 
     def test_plot_flux(self):
 
-        filtered = filter_skycomponents_by_flux(self.components, flux_min=0.1)
-        flux_in, flux_out = plot_skycomponents_flux(
-            filtered, self.components, plot_file=self.plot_file
+        comp_test = addnoise_skycomponent(
+            self.components, noise=self.noise, mode="flux"
         )
-      
-        assert_array_almost_equal(flux_in, flux_out)
-        assert len(flux_in) == len(filtered)
+
+        flux_in, flux_out = plot_skycomponents_flux(
+            comp_test, self.components, plot_file=self.plot_file
+        )
+
+        assert_array_almost_equal(flux_in, flux_out, decimal=3)
+        assert len(flux_in) == len(self.components)
+        assert len(flux_out) == len(comp_test)
 
     def test_plot_flux_ratio(self):
 
-        filtered = filter_skycomponents_by_flux(self.components, flux_min=0.1)
-        dist, flux_ratio = plot_skycomponents_flux_ratio(
-            filtered, self.components, self.phasecentre, plot_file=self.plot_file
+        comp_test = addnoise_skycomponent(
+            self.components, noise=self.noise, mode="flux"
         )
-        
-        assert_array_almost_equal(flux_ratio, 1.0)
-        assert len(flux_ratio) == len(filtered)
+        dist, flux_ratio = plot_skycomponents_flux_ratio(
+            comp_test, self.components, self.phasecentre, plot_file=self.plot_file
+        )
+
+        assert_array_almost_equal(flux_ratio, 1.0, decimal=3)
+        assert len(flux_ratio) == len(comp_test)
 
     def test_plot_flux_histogram(self):
 
-        filtered = filter_skycomponents_by_flux(self.components, flux_min=0.1)
-        [flux_in, flux_out] = plot_skycomponents_flux_histogram(
-            filtered, self.components, plot_file=self.plot_file
+        comp_test = addnoise_skycomponent(
+            self.components, noise=self.noise, mode="flux"
         )
-    
+        [flux_in, flux_out] = plot_skycomponents_flux_histogram(
+            comp_test, self.components, plot_file=self.plot_file
+        )
+
         assert len(flux_out) <= len(self.components)
-        assert len(flux_in) <= len(filtered)
+        assert len(flux_in) <= len(comp_test)
 
 
 if __name__ == "__main__":
