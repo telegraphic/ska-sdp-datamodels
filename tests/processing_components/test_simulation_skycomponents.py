2a1489cfa89819dec5dd5db74a16837a60ab7ae9
Celeste Lu <yl790@cam.ac.uk>
2021-03-25 13:20:09 +0000

    Added function to add noise to skycomponents and tests

---
+++ tests/processing_components/test_simulation_skycomponents.py
@@ -0,0 +1,73 @@
+"""Unit tests for functions in simulations/skycomponents.py
+
+"""
+
+import logging
+import sys
+import unittest
+
+import astropy.units as u
+import numpy
+from numpy.testing import assert_almost_equal
+from astropy.coordinates import SkyCoord
+
+from rascil.data_models import Skycomponent
+from rascil.data_models.polarisation import PolarisationFrame
+
+from rascil.processing_components.simulation.skycomponents import addnoise_skycomponent
+from rascil.processing_components.simulation import (
+    create_low_test_skycomponents_from_gleam,
+)
+from rascil.processing_components.skycomponent.base import copy_skycomponent
+
+log = logging.getLogger("rascil-logger")
+
+log.setLevel(logging.WARNING)
+log.addHandler(logging.StreamHandler(sys.stdout))
+
+
+class TestSimSkycomponents(unittest.TestCase):
+    def setUp(self):
+
+        from rascil.data_models.parameters import rascil_path, rascil_data_path
+
+        self.dir = rascil_path("test_results")
+
+        self.frequency = numpy.array([1e8])
+        self.phasecentre = SkyCoord(
+            ra=+30.0 * u.deg, dec=-60.0 * u.deg, frame="icrs", equinox="J2000"
+        )
+
+        self.noise = 1.0e-5
+
+    def test_addnoise_skycomponents(self):
+
+        self.components = create_low_test_skycomponents_from_gleam(
+            flux_limit=0.1,
+            phasecentre=self.phasecentre,
+            frequency=self.frequency,
+            polarisation_frame=PolarisationFrame("stokesI"),
+            radius=0.5,
+        )
+
+        original = copy_skycomponent(self.components)
+        self.components = addnoise_skycomponent(
+            self.components, noise=self.noise, mode="both"
+        )
+        ra_orig = numpy.array([comp.direction.ra.radian for comp in original])
+        dec_orig = numpy.array([comp.direction.dec.radian for comp in original])
+        flux_orig = numpy.array([comp.flux[0, 0] for comp in original])
+
+        ra_new = numpy.array([comp.direction.ra.radian for comp in self.components])
+        dec_new = numpy.array([comp.direction.dec.radian for comp in self.components])
+        flux_new = numpy.array([comp.flux[0, 0] for comp in self.components])
+
+        assert len(self.components) == len(original)
+
+        assert_almost_equal(numpy.mean(ra_orig), numpy.mean(ra_new), decimal=3)
+        assert_almost_equal(numpy.mean(dec_orig), numpy.mean(dec_new), decimal=3)
+        assert_almost_equal(numpy.mean(flux_orig), numpy.mean(flux_new), decimal=3)
+
+
+if __name__ == "__main__":
+    unittest.main()
