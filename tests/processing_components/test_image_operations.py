e2ef507c56549a8f0a544a3ec43537955782e9bf
Tim Cornwell <realtimcornwell@gmail.com>
2021-03-26 12:12:43 +0000

    Write/read clean beam to/from fits

--- tests/processing_components/test_image_operations.py
+++ tests/processing_components/test_image_operations.py
@@ -98,14 +98,20 @@ class TestImage(unittest.TestCase):
         m31model_by_array = create_image_from_array(
             self.m31image["pixels"],
             self.m31image.image_acc.wcs,
-            self.m31image.image_acc.polarisation_frame,
+            self.m31image.image_acc.polarisation_frame
         )
+        clean_beam = {"bmaj":0.1, "bmin":0.05, "bpa":-60.0}
+        self.m31image.attrs["clean_beam"] = clean_beam
+        
         add_image(self.m31image, m31model_by_array)
         assert m31model_by_array.image_acc.shape == self.m31image.image_acc.shape
-        if self.persist:
-            export_image_to_fits(
-                self.m31image, fitsfile="%s/test_model.fits" % (self.dir)
-            )
+        export_image_to_fits(
+            self.m31image, fitsfile="%s/test_model.fits" % (self.dir)
+        )
+        m31image_by_fits = import_image_from_fits(fitsfile="%s/test_model.fits" % (self.dir))
+        new_clean_beam = m31image_by_fits.attrs["clean_beam"]
+        assert new_clean_beam == clean_beam, new_clean_beam
+        
         log.debug(qa_image(m31model_by_array, context="test_create_from_image"))
 
     def test_create_empty_image_like(self):
