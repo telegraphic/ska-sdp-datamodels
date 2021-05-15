4f7a94a24c7270e1787c92d9052ac8efb372c347
Celeste Lu <yl790@cam.ac.uk>
2021-05-15 17:33:37 +0100

    Reorganized the tests

--- tests/apps/test_ci_checker_main_functions.py
+++ tests/apps/test_ci_checker_main_functions.py
@@ -42,6 +42,8 @@ log.setLevel(logging.WARNING)
 class TestCIChecker(unittest.TestCase):
     def setUp(self):
 
+        # Generate mock objects for functions in ci_checker,
+        # including skycomponents, primary beam image, restored image
         self.dir = rascil_path("test_results")
 
         self.persist = os.getenv("RASCIL_PERSIST", False)
@@ -54,41 +56,33 @@ class TestCIChecker(unittest.TestCase):
         )
         self.npixel = 512
         self.cellsize = 0.0001
-        self.bandwidth_single = numpy.array([1e7])
         self.bandwidth_array = numpy.full(self.nchan, 1e7)
 
+        # Below are parameters used to generate primary beam
         hwhm_deg, null_az_deg, null_el_deg = find_pb_width_null(
-            pbtype="MID", frequency=numpy.array([self.central_frequency])
+            pbtype="MID", frequency=self.central_frequency
         )
 
         hwhm = hwhm_deg * numpy.pi / 180.0
-        fov_deg = 8.0 * 1.36e9 / self.central_frequency
+        self.pbradius = 1.5 * hwhm
         self.pb_npixel = 256
+        fov_deg = 8.0 * 1.36e9 / self.central_frequency
         d2r = numpy.pi / 180.0
         self.pb_cellsize = d2r * fov_deg / self.pb_npixel
-        pbradius = 1.5
-        pbradius = pbradius * hwhm
-        flux_limit = 0.001
+        self.flux_limit = 0.001
 
+        # List of skycomponents to test
         self.components = create_mid_simulation_components(
             self.phasecentre,
             self.component_frequency,
-            flux_limit,
-            pbradius,
+            self.flux_limit,
+            self.pbradius,
             self.pb_npixel,
             self.pb_cellsize,
             apply_pb=False,
         )[0]
 
-        self.single_chan_image = create_image(
-            npixel=self.npixel,
-            cellsize=self.cellsize,
-            polarisation_frame=PolarisationFrame("stokesI"),
-            frequency=self.central_frequency,
-            channel_bandwidth=self.bandwidth_single,
-            phasecentre=self.phasecentre,
-        )
-
+        # Test restored image
         self.multi_chan_image = create_image(
             npixel=self.npixel,
             cellsize=self.cellsize,
@@ -103,17 +97,8 @@ class TestCIChecker(unittest.TestCase):
         )
         export_image_to_fits(self.multi_chan_image, self.restored_image_multi)
 
-        self.restored_image_single = (
-            self.dir + "/test_ci_checker_functions_nchan1_restored.fits"
-        )
-        export_image_to_fits(self.single_chan_image, self.restored_image_single)
+        self.txtfile = self.dir + "/test_ci_checker_functions.txt"
 
-        parser = cli_parser()
-        self.args = parser.parse_args([])
-
-    def test_correct_primary_beam(self):
-
-        # Test using sensitivity image
         pbmodel = create_image(
             npixel=self.pb_npixel,
             cellsize=self.pb_cellsize,
@@ -122,14 +107,24 @@ class TestCIChecker(unittest.TestCase):
             polarisation_frame=PolarisationFrame("stokesI"),
         )
 
-        pb = create_pb(pbmodel, "MID", pointingcentre=self.phasecentre, use_local=False)
-        components_with_pb = apply_beam_to_skycomponent(self.components, pb)
+        self.pb = create_pb(
+            pbmodel, "MID", pointingcentre=self.phasecentre, use_local=False
+        )
+        self.components_with_pb = apply_beam_to_skycomponent(self.components, self.pb)
+
+        parser = cli_parser()
+        self.args = parser.parse_args([])
+
+    def test_correct_primary_beam_sensitivity(self):
+
+        # Test using sensitivity image
+        # After adding and dividing primary beam, the flux should stay roughly the same
 
         sensitivity_image = self.dir + "/test_ci_checker_functions_sensitivity.fits"
-        export_image_to_fits(pb, sensitivity_image)
+        export_image_to_fits(self.pb, sensitivity_image)
 
         reversed_comp = correct_primary_beam(
-            None, sensitivity_image, components_with_pb, "MID"
+            None, sensitivity_image, self.components_with_pb, "MID"
         )
 
         orig_flux = [c.flux[self.nchan // 2][0] for c in self.components]
@@ -137,21 +132,26 @@ class TestCIChecker(unittest.TestCase):
 
         assert_array_almost_equal(orig_flux, reversed_flux, decimal=3)
 
+    def test_correct_primary_beam_restored(self):
+
         # Test using restored image
         reversed_comp_rest = correct_primary_beam(
-            self.restored_image_multi, None, components_with_pb, telescope="MID"
+            self.restored_image_multi, None, self.components_with_pb, telescope="MID"
         )
+        orig_flux = [c.flux[self.nchan // 2][0] for c in self.components]
         reversed_flux_rest = [c.flux[self.nchan // 2][0] for c in reversed_comp_rest]
 
         assert_array_almost_equal(orig_flux, reversed_flux_rest, decimal=1)
 
+    def _correct_primary_beam_none(self):
+
+        # If we don't feed either sensitivity or restored image, should return the same components back
         reversed_comp_none = correct_primary_beam(None, None, components_with_pb, "MID")
         assert components_with_pb == reversed_comp_none
 
-    def test_read_skycomponent_from_txt(self):
+    def test_read_skycomponent_from_txt_multi(self):
 
-        txtfile = self.dir + "/test_ci_checker_functions.txt"
-        f = open(txtfile, "w")
+        f = open(self.txtfile, "w")
         f.write(
             "# RA(deg), Dec(deg), I (Jy), Q (Jy), U (Jy), V (Jy), Ref. freq. (Hz), Spectral index\n"
         )
@@ -174,17 +174,26 @@ class TestCIChecker(unittest.TestCase):
             )
         f.close()
 
-        components_read = read_skycomponent_from_txt(txtfile, self.component_frequency)
+        components_read = read_skycomponent_from_txt(
+            self.txtfile, self.component_frequency
+        )
         assert len(components_read) == len(self.components)
         orig_ra = [c.direction.ra.degree for c in self.components]
         read_ra = [c.direction.ra.degree for c in components_read]
 
         assert_array_almost_equal(orig_ra, read_ra)
 
+    def test_read_skycomponent_from_txt_single(self):
+
         # Test single channel components
+        # The flux for each component should only have one value
         components_read_single = read_skycomponent_from_txt(
-            txtfile, self.central_frequency
+            self.txtfile, self.central_frequency
         )
+
+        for c in components_read_single:
+            assert c.flux.shape == (1, 1)
+
         orig_flux = [c.flux[self.nchan // 2][0] for c in self.components]
         read_flux = [c.flux[0][0] for c in components_read_single]
 
