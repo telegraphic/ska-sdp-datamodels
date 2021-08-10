150d8bf907e8b9ef0caadb59ca411e5f95e4f4f1
Tim Cornwell <realtimcornwell@gmail.com>
2021-08-10 12:06:38 +0100

    SIM-986: Address Celeste's review
    Also added ability to specify components

--- tests/apps/test_rascil_rcal.py
+++ tests/apps/test_rascil_rcal.py
@@ -1,4 +1,4 @@
-""" Unit processing_components for rascil advise
+""" Unit test for rascil_rcal app.
 
 """
 import sys
@@ -12,7 +12,12 @@ from astropy import units as u
 from astropy.coordinates import SkyCoord
 
 from rascil.apps.rascil_rcal import cli_parser, rcal_simulator
-from rascil.data_models import rascil_path, Skycomponent, import_gaintable_from_hdf5
+from rascil.data_models import (
+    rascil_path,
+    Skycomponent,
+    import_gaintable_from_hdf5,
+    export_skycomponent_to_hdf5,
+)
 from rascil.data_models.polarisation import PolarisationFrame
 from rascil.processing_components import (
     export_blockvisibility_to_ms,
@@ -21,6 +26,7 @@ from rascil.processing_components import (
     simulate_gaintable,
     apply_gaintable,
     qa_visibility,
+    qa_gaintable,
 )
 from rascil.processing_components.simulation import create_named_configuration
 from rascil.processing_components.simulation import ingest_unittest_visibility
@@ -30,8 +36,14 @@ log.setLevel(logging.INFO)
 log.addHandler(logging.StreamHandler(sys.stdout))
 
 
-class TestRASCILRCAL(unittest.TestCase):
-    def make_MS(self):
+class TestRASCILRcal(unittest.TestCase):
+    def make_MS(self, dopol=False):
+        """Create and fill values into the MeassurementSet
+
+        :param dopol: Use polarisation?
+        """
+
+        self.cleanup_data_files()
 
         self.low = create_named_configuration("LOW-AA0.5")
         self.freqwin = 200
@@ -46,28 +58,20 @@ class TestRASCILRCAL(unittest.TestCase):
         else:
             self.channelwidth = numpy.array([1e6])
 
-        dopol = False
         if dopol:
             self.vis_pol = PolarisationFrame("linear")
             self.image_pol = PolarisationFrame("stokesIQUV")
-            f = [1.0, 0.2, 0.0, 0.0]
+            f = [100.0, 20.0, 0.0, 0.0]
         else:
             self.vis_pol = PolarisationFrame("stokesI")
             self.image_pol = PolarisationFrame("stokesI")
-            f = [1.0]
+            f = [100.0]
 
         flux = numpy.array(self.freqwin * [f])
 
         self.phasecentre = SkyCoord(
             ra=+180.0 * u.deg, dec=-60.0 * u.deg, frame="icrs", equinox="J2000"
         )
-        pointsource = Skycomponent(
-            direction=self.phasecentre,
-            polarisation_frame=self.image_pol,
-            flux=flux,
-            frequency=self.frequency,
-        )
-
         self.bvis_original = ingest_unittest_visibility(
             self.low,
             self.frequency,
@@ -77,23 +81,57 @@ class TestRASCILRCAL(unittest.TestCase):
             self.phasecentre,
         )
 
+        self.create_dft_components(flux)
+
+        self.create_apply_gains()
+
+    def create_dft_components(self, flux):
+        """Create the components, save to file, dft into visibility
+
+        :param flux:
+        """
+        pointsource = Skycomponent(
+            direction=self.phasecentre,
+            polarisation_frame=self.image_pol,
+            flux=flux,
+            frequency=self.frequency,
+        )
         self.bvis_original = dft_skycomponent_visibility(
             self.bvis_original, pointsource
         )
+        export_skycomponent_to_hdf5(
+            [pointsource],
+            rascil_path("test_results/test_rascil_rcal_components.hdf"),
+        )
 
+    def create_apply_gains(self):
+        """Create the gaintable, apply to the visibility, write as MeasurementSet
+
+        :return:
+        """
         self.gt = create_gaintable_from_blockvisibility(self.bvis_original)
         self.gt = simulate_gaintable(self.gt, phase_error=0.1)
+        qa_gt = qa_gaintable(self.gt)
+        assert qa_gt.data["rms-amp"] < 1e-12, str(qa_gt)
+        assert qa_gt.data["rms-phase"] > 0.0, str(qa_gt)
         self.bvis_error = apply_gaintable(self.bvis_original, self.gt)
+        assert numpy.std(numpy.angle(self.bvis_error["vis"].data)) > 0.0
+        export_blockvisibility_to_ms(
+            rascil_path("test_results/test_rascil_rcal.ms"), [self.bvis_error]
+        )
 
+    def cleanup_data_files(self):
+        """Cleanup the temporary data files"""
         shutil.rmtree(
-            rascil_path("test_results/test_rascil_rcal_gaintable.hdf5"),
+            rascil_path("test_results/test_rascil_rcal_components.hdf"),
+            ignore_errors=True,
+        )
+        shutil.rmtree(
+            rascil_path("test_results/test_rascil_rcal_gaintable.hdf"),
             ignore_errors=True,
         )
         shutil.rmtree(
             rascil_path("test_results/test_rascil_rcal.ms"), ignore_errors=True
-        )
-        export_blockvisibility_to_ms(
-            rascil_path("test_results/test_rascil_rcal.ms"), [self.bvis_error]
         )
 
     def setUp(self) -> None:
@@ -101,23 +139,27 @@ class TestRASCILRCAL(unittest.TestCase):
         parser = cli_parser()
         self.args = parser.parse_args([])
         self.args.ingest_msname = rascil_path("test_results/test_rascil_rcal.ms")
+        self.args.ingest_components_file = rascil_path(
+            "test_results/test_rascil_rcal_components.hdf"
+        )
 
     def tearDown(self) -> None:
-        shutil.rmtree(
-            rascil_path("test_results/test_rascil_rcal.ms"), ignore_errors=True
-        )
-        shutil.rmtree(
-            rascil_path("test_results/test_rascil_rcal_gaintable.hdf5"),
-            ignore_errors=True,
-        )
+        self.cleanup_data_files()
 
     def test_rcal(self):
 
         self.make_MS()
         gtfile = rcal_simulator(self.args)
+
+        # Check that the gaintable exists and is correct by applying it to
+        # the corrupted visibility
         assert os.path.exists(gtfile)
         newgt = import_gaintable_from_hdf5(gtfile)
-        log.info(f"Gaintable: {newgt}")
+        log.info(f"\nFinal gaintable: {newgt}")
+
+        qa_gt = qa_gaintable(newgt)
+        log.info(qa_gt)
+        assert qa_gt.data["rms-phase"] > 0.0, str(qa_gt)
 
         bvis_difference = apply_gaintable(self.bvis_error, newgt, inverse=True)
         bvis_difference["vis"] -= self.bvis_original["vis"]
