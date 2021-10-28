8c2e52436bf87df797fd7eb46493e8d8cb8f6d05
Celeste Lu <yl790@cam.ac.uk>
2021-10-28 23:53:35 +0100

    Added test for reading txt function

--- tests/apps/test_rascil_imager_functions.py
+++ tests/apps/test_rascil_imager_functions.py
@@ -13,6 +13,38 @@ from rascil.data_models import (
     PolarisationFrame,
 )
 from rascil.processing_components.image.operations import create_image
+from rascil.processing_components.skycomponent import fit_skycomponent_spectral_index
+
+
+def write_to_txt(filename, components):
+
+    f = open(filename, "w")
+    f.write(
+        "# RA(deg), Dec(deg), I (Jy), Q (Jy), U (Jy), V (Jy), Ref. freq. (Hz), Spectral index\n"
+    )
+
+    for cmp in components:
+        coord_ra = cmp.direction.ra.degree
+        coord_dec = cmp.direction.dec.degree
+        spec_indx = fit_skycomponent_spectral_index(cmp)
+        nchan = len(cmp.frequency)
+        central_freq = cmp.frequency[nchan // 2]
+        f.write(
+            "%.6f, %.6f, %10.6e, %10.6e, %10.6e, %10.6e, %10.6e, %10.6e \n"
+            % (
+                coord_ra,
+                coord_dec,
+                cmp.flux[nchan // 2],
+                0.0,
+                0.0,
+                0.0,
+                central_freq,
+                spec_indx,
+            )
+        )
+    f.close()
+
+    return
 
 
 @pytest.fixture(scope="module")
@@ -49,9 +81,13 @@ def sky_comp_file():
     skycomp_file = rascil_path("test_results/test_generate_skymodel_list_skycomps.hdf5")
     export_skycomponent_to_hdf5(sky_com_list, skycomp_file)
 
-    yield skycomp_file
+    skycomp_txt = rascil_path("test_results/test_generate_skymodel_list_skycomps.txt")
+    write_to_txt(skycomp_txt, sky_com_list)
+
+    yield skycomp_file, skycomp_txt
 
     os.remove(skycomp_file)
+    os.remove(skycomp_txt)
 
 
 @pytest.fixture(scope="module")
@@ -104,7 +140,7 @@ def test_generate_skymodel_list_from_hdf_one_comp(mock_image, sky_comp_file):
     Note: frequency of image and components may not match!
     """
     result = generate_skymodel_list(
-        [mock_image[0]], input_file=sky_comp_file, n_bright_sources=1
+        [mock_image[0]], input_file=sky_comp_file[0], n_bright_sources=1
     )
 
     assert len(result) == 1
@@ -117,7 +153,7 @@ def test_generate_skymodel_list_from_hdf_two_comps(mock_image, sky_comp_file):
     Note: frequency of image and components may not match!
     """
     result = generate_skymodel_list(
-        [mock_image[0]], input_file=sky_comp_file, n_bright_sources=2
+        [mock_image[0]], input_file=sky_comp_file[0], n_bright_sources=2
     )
 
     assert len(result) == 1
@@ -131,10 +167,8 @@ def test_generate_skymodel_list_from_hdf_all_comps(mock_image, sky_comp_file):
     Note: frequency of image and components may not match!
     """
     result = generate_skymodel_list(
-        [mock_image[0]], input_file=sky_comp_file, n_bright_sources=None
+        [mock_image[0]], input_file=sky_comp_file[0], n_bright_sources=None
     )
-
-    print(result[0].components[1].flux)
 
     assert len(result) == 1
     assert len(result[0].components) == 3
@@ -142,3 +176,56 @@ def test_generate_skymodel_list_from_hdf_all_comps(mock_image, sky_comp_file):
     assert (result[0].components[1].flux == numpy.array([[1.1], [2.2], [2.5]])).all()
     assert (result[0].components[0].flux == numpy.array([[1.0], [2.0], [2.5]])).all()
     assert (result[0].components[2].flux == numpy.array([[0.4], [1.1], [1.6]])).all()
+
+
+def test_generate_skymodel_list_from_txt_one_comp(mock_image, sky_comp_file):
+    """
+    Note: frequency of components are scaled to image frequency
+    """
+    result = generate_skymodel_list(
+        [mock_image[0]], input_file=sky_comp_file[1], n_bright_sources=1
+    )
+
+    expected = numpy.array([[1.34210651], [1.35848315], [1.37489533]])
+    assert len(result) == 1
+    assert len(result[0].components) == 1
+    assert_almost_equal(result[0].components[0].flux, expected)
+
+
+def test_generate_skymodel_list_from_txt_two_comps(mock_image, sky_comp_file):
+    """
+    Note: frequency of components are scaled to image frequency
+    """
+    result = generate_skymodel_list(
+        [mock_image[0]], input_file=sky_comp_file[1], n_bright_sources=2
+    )
+
+    expected1 = numpy.array([[1.34210651], [1.35848315], [1.37489533]])
+    expected2 = numpy.array([[1.15816502], [1.1737968], [1.18948243]])
+
+    assert len(result) == 1
+    assert len(result[0].components) == 2
+    assert_almost_equal(result[0].components[0].flux, expected1)
+    assert_almost_equal(result[0].components[1].flux, expected2)
+
+
+def test_generate_skymodel_list_from_txt_all_comps(mock_image, sky_comp_file):
+    """
+    Note: frequency of components are scaled to image frequency
+    """
+    result = generate_skymodel_list(
+        [mock_image[0]],
+        input_file=sky_comp_file[1],
+        n_bright_sources=None,
+    )
+
+    expected1 = numpy.array([[1.34210651], [1.35848315], [1.37489533]])
+    expected2 = numpy.array([[1.15816502], [1.1737968], [1.18948243]])
+    expected3 = numpy.array([[0.48249189], [0.49234909], [0.50230758]])
+
+    assert len(result) == 1
+    assert len(result[0].components) == 3
+    # The flux are not sorted
+    assert_almost_equal(result[0].components[0].flux, expected2)
+    assert_almost_equal(result[0].components[1].flux, expected1)
+    assert_almost_equal(result[0].components[2].flux, expected3)
