5522742a555373219cc23ec7d9ee5160a8871b3e
cnwangfeng <cnwangfeng@gmail.com>
2022-10-15 14:40:55 +0800

    ORC-1488 temp comment divide_visibility

--- tests/processing_components/test_visibility_operations.py
+++ tests/processing_components/test_visibility_operations.py
@@ -212,7 +212,9 @@ class TestVisibilityOperations(unittest.TestCase):
         )
         vis = copy_visibility(self.vis)
         self.vis["vis"][...] = 0.0
+        # pylint: disable=unsupported-assignment-operation
         vis["vis"][...] = 1.0
+        # pylint: disable=unsubscriptable-object
         assert vis["vis"][0, 0].real.all() == 1.0
         assert self.vis["vis"][0, 0].real.all() == 0.0
 
