230f5f94642dd6d761a8d5bff4de329679578f57
Dominic Schaff <dominic@schaff.dev>
2023-07-22 15:47:39 +0200

    NAL-648 Add new encode/decode and upgrade xarray
    NAL-648 Regenerate lock file

---
+++ src/ska_sdp_datamodels/utilities.py
@@ -0,0 +1,59 @@
+"""This module contains an encode function to convert xarray.Dataset based
+objects into a msgpack bytes object. Note that currently the decode does not
+convert directly back to what the orriginal object was.
+
+This has been tested on the current Visibility object."""
+
+import msgpack
+import msgpack_numpy
+
+try:
+    import pyarrow
+
+    PYARROW_AVAILABLE = True
+except ModuleNotFoundError:
+    PYARROW_AVAILABLE = False
+import xarray
+from astropy.coordinates import SkyCoord
+
+from ska_sdp_datamodels.science_data_model import ReceptorFrame
+
+
+def _dataset_encoder(obj):
+    """Custom encoder for datamodel specific code."""
+
+    # Dataset to dict conversion:
+    if isinstance(obj, xarray.Dataset):
+        out_dict = obj.to_dict(data="array")
+
+        # we need to remove the datetime key, as this cannot be encoded
+        if "datetime" in out_dict["data_vars"]:
+            del out_dict["data_vars"]["datetime"]
+
+        return out_dict
+
+    # A method to get the ReceptorFrame object to encode
+    if isinstance(obj, ReceptorFrame):
+        return obj.names
+
+    # A method to get the SkyCoord to convert
+    if isinstance(obj, SkyCoord):
+        return str(obj)
+
+    # Convert the Table type to a list
+    if PYARROW_AVAILABLE and isinstance(obj, pyarrow.lib.Table):
+        return obj.to_pylist()
+
+    # Default to attempting to convert assuming numpy data
+    return msgpack_numpy.encode(obj)
+
+
+def encode(dataset: xarray.Dataset) -> bytes:
+    """Encode a Dataset object into a msgpack bytes."""
+    return msgpack.packb(dataset, default=_dataset_encoder)
+
+
+def decode(bytes_dataset: bytes) -> xarray.Dataset:
+    """Decode a msgpack bytes to a Dataset object."""
+    data_raw = msgpack.unpackb(bytes_dataset)
+    return xarray.Dataset.from_dict(data_raw)
